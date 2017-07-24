615178a50db2e93279dcc0651e8d124e7322876c
300930741 <300930741@PR-A315-39>
2017-07-24 19:59:07 -0400

    created abstract class plenet

---
+++ Assignment_Abstract Planet/Planet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Name: Robin singh
+ * Date: July 12, 2017
+ * Description: This is the abstract class of planet
+ * Version: 0.1- Created planet class
+ */
+namespace Assignment_Abstract_Planet
+{
+
+    // <summary>
+    // This is the Planet Abstract class
+    // </summary>
+
+        public abstract class Planet
+    {
+        // PRIVATE FIELD (INSTANCE VARIABLES)
+        private double _diameter;
+
+        //Public PROPERTIES
+    }
+   
+    
+    
+}
--- Assignment_Abstract Planet/Program.cs
+++ Assignment_Abstract Planet/Program.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * Name: Robin singh
+ * Date: July 12, 2017
+ * Description: This is the abstract class
+ * Version: 0.2 - Added Public Abstract method DisplaySkills
+ */
 namespace Assignment_Abstract_Planet
 {
+
     class Program
     {
         static void Main(string[] args)
