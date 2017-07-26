c1c2bf6ca10e6348b2a5e773920fd76fe27d2f80
300930741 <300930741@PR-A315-39>
2017-07-26 18:34:11 -0400

    Created PRIVATE INSTANCE VARIABLES and MAIN CONSTRUCTOR

---
+++ Assignment_Abstract Planet/TerrestrialPlanet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: Robin singh
+ * Date: July 12, 2017
+ * Description: This is the abstract class of TerrestialPlanet
+ * Version: 0.1- Created PRIVATE INSTANCE VARIABLES and MAIN CONSTRUCTOR   
+ */
+namespace Assignment_Abstract_Planet
+{
+    class TerrestrialPlanet : Planet
+    {
+        // PRIVATE INSTANCE VARIABLES (FIELDS)
+        private bool _oxygen;
+
+        //CONSTRUCTOR
+
+        /// <summary>
+        /// This is the main CONSTRUCTOR for the class TerrestrialPlanet.
+        /// It takes four parameters - name(string) - diameter(double) - mass(double) - oxygen(bool).
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="diameter"></param>
+        /// <param name="mass"></param>
+        /// <param name="oxygen"></param>
+        public TerrestrialPlanet(string name, double diameter, double mass, bool oxygen)
+                : base(name, diameter, mass)
+        {
+            this._oxygen = oxygen;
+        }
+    }
+}
