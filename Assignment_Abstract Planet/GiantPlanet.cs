8fe88d656229454d848f073440be2ef130b21c9e
300930741 <300930741@PR-A315-39>
2017-07-26 18:10:31 -0400

    Created PRIVATE INSTANCE VARIABLES and CONSTRUCTOR CLASS

---
+++ Assignment_Abstract Planet/GiantPlanet.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: Robin singh
+ * Date: July 12, 2017
+ * Description: This is the abstract class of GiantPlanet
+ * Version: 0.1- Created PRIVATE INSTANCE VARIABLES and CONSTRUCTOR CLASS
+ */
+namespace Assignment_Abstract_Planet
+{
+    class GiantPlanet : Planet
+    { // PRIVATE INSTANCE VARIABLES (FIELDS)
+        private string _type;
+
+
+        //CONSTRUCTOR
+
+        /// <summary>
+        /// This is the main constructor for the GiantPlanet class.
+        /// It takes four parameters - name(string) - diameter(double) - mass(double) - type(string).
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="diameter"></param>
+        /// <param name="mass"></param>
+        /// <param name="type"></param>
+        public GiantPlanet(string name, double diameter, double mass, string type)
+                : base(name, diameter, mass)
+        {
+            this._type = type;
+        }
+
+        
+
+    }
+}
