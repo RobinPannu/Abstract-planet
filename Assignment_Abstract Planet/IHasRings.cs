85946a57644fd3447f3de468693bc4a00cbe3e5b
300930741 <300930741@PR-A315-39>
2017-07-27 20:48:06 -0400

    Created  interface IHasRings

---
+++ Assignment_Abstract Planet/IHasRings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Name: Robin singh
+ * Date: July 27, 2017
+ * Description: This is the interface that define Ring Property
+ * Version: 0.1- Created  interface IHasRings
+ */
+namespace Assignment_Abstract_Planet
+{
+   public interface IHasRings
+    {
+        //PUBLIC METHOD
+        bool HasRings();
+    }
+}
