9fecf2da8ddf88217cb908bd5480d4418057e2c3
300930741 <300930741@PR-A315-39>
2017-07-27 20:54:41 -0400

    Created  interface IHabitable

---
+++ Assignment_Abstract Planet/IHabitable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+ * Name: Robin singh
+ * Date: July 27, 2017
+ * Description: This is the interface that define Habitble Property
+ * Version: 0.1- Created  interface IHabitable
+ */
+namespace Assignment_Abstract_Planet
+{
+    interface IHabitable
+    {
+        bool Habitable();
+    }
+}
